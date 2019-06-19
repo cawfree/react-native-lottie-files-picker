d90020390c70bc0a383f3ba6d2d7c1e67ca6890a
@cawfree <cawfree@gmail.com>
2019-06-19 15:04:26 +0100

    improved icon layout

---
+++ example/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import LottieFilesPicker from './LottieFilesPicker';
+
+const styles = StyleSheet.create({
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <LottieFilesPicker
+      />
+    );
+  }
+}
